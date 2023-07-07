70290c9a441bbe5f53336f65041d3139c8b69e1c
2amlover <126642452+2amlover@users.noreply.github.com>
2023-07-07 12:47:26 +0530

    added images to summerwear.jsx

--- src/Pages/SummerWear/Summerwear.jsx
+++ src/Pages/SummerWear/Summerwear.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import './summerwear.css';
+import Summer1 from '../../Assets/Images/Summer1.jpg';
+import Summer2 from '../../Assets/Images/Summer2.jpg';
+import Summer3 from '../../Assets/Images/Summer3.jpg';
+import Summer4 from '../../Assets/Images/Summer4.jpg';
 
 const Summerwear = () => {
   const cards = [
-    { id: 1, imageUrl: '', title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
-    { id: 2, imageUrl: '', title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
-    { id: 3, imageUrl: '', title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
-    { id: 4, imageUrl: '', title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
+    { id: 1, imageUrl: Summer1 , title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
+    { id: 2, imageUrl: Summer2, title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
+    { id: 3, imageUrl: Summer3, title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
+    { id: 4, imageUrl: Summer4, title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
+    { id: 1, imageUrl: Summer1, title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
+    { id: 2, imageUrl: Summer2, title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
+    { id: 3, imageUrl: Summer3, title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
+    { id: 4, imageUrl: Summer4, title: 'T-Shirt', description: 'For cool Personolity', price:'$ 599'},
   ];
 
   return (
-    <div>
-    <div className="Filetr">Filter </div>
      <div className="summerwear-container">
       {cards.map(card => (
         <div className="card" key={card.id}>
-          <img src={card.imageUrl} alt={card.title} />
+          <img src={card.imageUrl} />
           <h3>{card.title}</h3>
           <p>{card.description}</p>
           <p>{card.price}</p>
         </div>
       ))}
      </div>
-    </div>
-   
   );
 };
 
